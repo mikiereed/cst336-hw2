4ff7526df816e043e3b00d6f4e28fc20cc86213d
EC2 Default User <ec2-user@ip-172-31-48-179.us-west-2.compute.internal>
2019-11-06 03:58:54 +0000

    added css, some js

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-      <title>US Quiz</title>
+      <title>Rock, Paper, Scissors</title>
       <link href="css/styles.css" rel="stylesheet" type="text/css" />
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min.js"></script>
@@ -13,17 +13,20 @@
         
         <h3>Rock, Paper, or Scissors?</h3>
         <input type="text" id="usersMove">
+        <h3 id="validationFdbk" class="danger"></h3>
+        <button id="shootButton">Shoot!</button>
         <br><br>
-
-        <div id="outcomeFeedback"></div>
-        <br>
-        
-        
-        <h3 id="validationFdbk" class="bg-danger text-white"></h3>
-        <button class="btn btn-outline-success">Submit Quiz</button>
-        <br>
-        <h2 id="totalScore"></h2>
-        <h3 id="goodScoreMessage" class="text-info"></h3>
-        <h3 id="totalAttempts"></h3>
+        <table>
+          <tr>
+            <td>You</td>
+            <td>Computer</td>
+          </tr>
+          <tr>
+            <td><img src="img/blank.png"></img></td>
+            <td><img src="img/blank.png"></img></td>
+          </tr>
+        </table>
+        <h2 id="outcome"></h2>
+        <h3 id="outcomeMessage"></h3>
     </body>
 </html>
